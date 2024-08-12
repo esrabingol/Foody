b99c73405806a6fb1731fb1b276e8d34f6be47cd
Songul Esra Bingol <songul90441@ogr.duzce.edu.tr>
2024-08-12 18:50:18 +0300

    lesson 20

--- Foody.WEBUI/Controllers/CategoriesController.cs
+++ Foody.WEBUI/Controllers/CategoriesController.cs
@@ -30,5 +30,25 @@ namespace Foody.WEBUI.Controllers
 			return RedirectToAction("CategoryList");
 		}
 
+		public IActionResult DeleteCategory(int id)
+		{
+			_categoryService.TDelete(id);
+			return RedirectToAction("Categorylist");
+
+		}
+
+		[HttpGet]
+		public IActionResult UpdateCategory(int id)
+		{
+			var value = _categoryService.TGetById(id);
+			return View(value);
+		
+		}
+		[HttpPost]
+		public IActionResult UpdateCategory(Category category)
+		{ 
+			_categoryService.TUpdate(category);
+			return RedirectToAction("CategoryList");
+		}
 	}
 }
