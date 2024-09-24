6182324853566a0431a5c0ffda3a8c479bca3251
Songul Esra Bingol <songul90441@ogr.duzce.edu.tr>
2024-09-24 14:35:37 +0300

    lesson 32

---
+++ Foody.WEBUI/Controllers/ErrorPagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.WEBUI.Controllers
+{
+	public class ErrorPagesController : Controller
+	{
+		public IActionResult ErrorPage404()
+		{
+			return View();
+		}
+	}
+}
