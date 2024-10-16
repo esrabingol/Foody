37b996c838dd7c544b5d65475b14d6160ef7875d
Songul Esra Bingol <songul90441@ogr.duzce.edu.tr>
2024-10-16 11:46:40 +0300

    lessons comp.

---
+++ Foody.WEBUI/ViewComponents/DefaultViewComponents/_DefaultNavbarSectionComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.WEBUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultNavbarSectionComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Foody.WEBUI/ViewComponents/DefaultViewComponents/_DefaultSliderSectionComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using Foody.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.WEBUI.ViewComponents.DefaultViewComponents
+{
+	public class _DefaultSliderSectionComponentPartial : ViewComponent
+	{
+		private readonly ISliderService _sliderService;
+		public _DefaultSliderSectionComponentPartial(ISliderService sliderService)
+		{
+			_sliderService = sliderService;
+		}
+		public IViewComponentResult Invoke()
+		{
+			var values = _sliderService.TGetAll();
+			return View(values);
+		}
+	}
+}
